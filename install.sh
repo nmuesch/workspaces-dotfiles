8a2aca563ac6e9712eb9e067a7567aedd4b3dde8
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2025-05-23 11:19:48 -0400

    add gitconfig and make install.sh setup the symlinks

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-# Setup FZF
+set -euo pipefail
+
+
+echo "Setting up Dotfiles Symlinks"
+DOTFILES_PATH="$HOME/dotfiles"
+# Symlink dotfiles to the root within your workspace
+find $DOTFILES_PATH -type f -path "$DOTFILES_PATH/.*" |
+while read df; do
+  link=${df/$DOTFILES_PATH/$HOME}
+  mkdir -p "$(dirname "$link")"
+  ln -sf "$df" "$link"
+done
+
 echo "Setting up FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc --no-fish
 
-# Setup Starship
 echo "Setting up Starship"
 touch ~/.zshrc
 curl -sS https://starship.rs/install.sh | sh -s -- -y
