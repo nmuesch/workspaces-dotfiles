a3dbd4bd5587c5f8f524dc7b227dca043751a02e
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2025-05-23 09:54:22 -0400

    Add log lines and setup starship too

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 # Setup FZF
+echo "Setting up FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc --no-fish
+
+# Setup Starship
+echo "Setting up Starship"
+touch ~/.zshrc
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+sed -i '$a\'$'\n''eval "$(starship init zsh)"' ~/.zshrc
+source ~/.zshrc
+starship preset plain-text-symbols -o ~/.config/starship.toml
