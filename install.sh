31f2444082f4f976004c0dddf6334081b62f9124
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2025-05-23 13:37:29 -0400

    Setup keyboard completions for FZF

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 set -euo pipefail
 
+append_to_zshrc() {
+  sed -i -e "\$a\\
+$1" ~/.zshrc
+}
 
 echo "Setting up Dotfiles Symlinks"
 DOTFILES_PATH="$HOME/dotfiles"
@@ -14,10 +18,13 @@ done
 echo "Setting up FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc --no-fish
+append_to_zshrc "source /usr/share/doc/fzf/examples/key-bindings.zsh"
+append_to_zshrc "source /usr/share/doc/fzf/examples/completion.zsh"
 
 echo "Setting up Starship"
 touch ~/.zshrc
 curl -sS https://starship.rs/install.sh | sh -s -- -y
-sed -i '$a\'$'\n''eval "$(starship init zsh)"' ~/.zshrc
+append_to_zshrc 'eval "$(starship init zsh)"'
 source ~/.zshrc
 starship preset plain-text-symbols -o ~/.config/starship.toml
+
